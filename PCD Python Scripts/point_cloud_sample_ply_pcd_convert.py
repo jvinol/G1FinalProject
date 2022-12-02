3561f11bbbb1e699c3b33f26f4d6e96d3850ce34
Jonas Fernandes <112453103+jvinol@users.noreply.github.com>
2022-12-01 23:18:22 -0600

    Update point_cloud_sample_ply_pcd_convert.py

--- PCD Python Scripts/point_cloud_sample_ply_pcd_convert.py
+++ PCD Python Scripts/point_cloud_sample_ply_pcd_convert.py
@@ -4,4 +4,4 @@ import numpy as np
 import open3d as o3d
 
 pcd = o3d.io.read_point_cloud("apartment.ply") # Change this filename if using a different ply layout
-o3d.io.write_point_cloud("apartment.pcd", pcd)
+o3d.io.write_point_cloud("apartment.pcd", pcd) # Change this filename to the name you want to save it as. Will need this to run the point_cloud_sample_pcd.py file
