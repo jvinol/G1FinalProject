64b43f0b2eb919c211955583357f7d6d1bb9cd11
Jonas Fernandes <112453103+jvinol@users.noreply.github.com>
2022-12-01 23:07:44 -0600

    Update point_cloud_sample_ply.py

--- PCD Python Scripts/point_cloud_sample_ply.py
+++ PCD Python Scripts/point_cloud_sample_ply.py
@@ -1,5 +1,5 @@
 import numpy as np
 import pyvista as pv
 
-point_cloud = pv.read('apt.ply')
+point_cloud = pv.read('apartment.ply')
 point_cloud.plot(eye_dome_lighting=True, window_size=[2000,1500])
